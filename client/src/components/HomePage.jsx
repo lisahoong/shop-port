509ec41568d6fd2428a586138483ee906abb0789
lisahoong <lhoong94@gmail.com>
2017-04-03 16:37:52 -0700

    updated pop up CSS

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,10 +4,10 @@ const HomePage = () => (
   <div className='container'>
     <div className="txt-img">
       <img src="./images/landing.png" alt=""/>
-      <h2>Welcome child. jajajajaja<br/>
-      <w>Please buy some things from us.</w></h2>
+      <h2>Welcome child.<br/>
+      <w>Please buy some things from us so Sebastian can stay in the U.S.</w></h2>
     </div>
-    <z id="top">jijij You must be wondering why there is empty space here.</z>
+    <z id="top">You must be wondering why there is empty space here.</z>
     <w>I'm not sure either but text is better than random white space.</w>
   </div>
 );
