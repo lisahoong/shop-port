d70d4e322d3f44ee4bba0d42e69229b76965d8f7
lisahoong <lhoong94@gmail.com>
2017-04-07 12:05:57 -0700

    added remove button for cart items

--- client/src/components/CartItems.jsx
+++ client/src/components/CartItems.jsx
@@ -1,14 +1,17 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
+
 const CartItems = function(props) {
   return (<div className="cart-100">
     <w>{props.userName}&#39;s Cart</w>
     {props.userItems.map(function(item, i) {
-      return (<div key={i} className="cart-useritems-container">
+      return ( <div key={i} className="cart-useritems-container">
           <div className="cart-userimage-desc-containter">
-        <img className="cart-userimage" href={item.link} src={item.src}/>
-      <a className="cart-userinfo" href={item.link}>{item.title}</a>
+      <div><img className="cart-userimage" href={item.link} src={item.src}/></div>
+      <div><a className="cart-userinfo" href={item.link}>{item.title}</a>
+      <button className="remove-button" id="remove" onClick={props.remove}>REMOVE</button>
+      </div>
       </div>
       <p className="cart-userprice">{item.price}</p>
 
